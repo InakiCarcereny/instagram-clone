a960e271c26fc41de6276d7ad36d61b818f6ca79
inakicarcereny <inakicarce@hotmail.com.ar>
2024-06-18 11:17:45 -0300

    icons

---
+++ src/icons/Save.tsx
@@ -0,0 +1,9 @@
+export function Save () {
+
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M18 7v14l-6 -4l-6 4v-14a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4z" />
+    </svg>
+  )
+}
